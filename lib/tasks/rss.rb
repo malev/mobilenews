91a113bd29667f80a9668f91af57ff8e213d0296
Marcos Vanetta <marcosvanetta@gmail.com>
2011-11-02 02:01:02 -0300

    working with rss

--- lib/tasks/rss.rb
+++ lib/tasks/rss.rb
@@ -21,16 +21,72 @@ class Source
 
 end
 
-class Pagina12Crawler
+class Pagina12
+  def initialize(url)
+    @url = url
+    @rss = SimpleRSS.parse open(url)
+    @items = rss.items
+  end
+
+  def get_header
+    { :title => "Pagina 12" }
+  end
+
+  def generate_header
+    ""
+  end
+
+  def generate_index
+    output = "<div class='index'>"
+    @items.each_with_index do |item, i|
+      resume = Resume.new item
+      output << resume.to_html("anchor_#{i}")
+    end
+    output << "</div>"
+  end
+
+  def generate_content
+    output = "<div class='content'>"
+    @items.each_with_index do |item, i|
+      article = ArticleCrawler.new(item["link"])
+      output << article.to_html("anchor_#{i}")
+    end
+    output << "</div>"
+  end
+
+  def to_html
+    output = "<html>"
+    output << generate_header + generate_index + generate_content
+    output << "</html>"
+  end
+end
+
+class Resume
+  def initialize(item)
+    @item = item
+  end
+
+  def to_html(link='')
+  end
+end
+
+class ArticleCrawler
   def initialize(link)
     @link = link
     @doc = Nokogiri::HTML(open(@link))
   end
 
   def generate_article
+    @doc.channel.link
   end
 
+  def generate_content
+  end
 end
 
 class Article
+  attr_accessor :title, :section, :author, :volanta, :content, :image
+
+  def to_html(link = '')
+  end
 end
