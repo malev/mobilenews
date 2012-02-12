2988ce6f25efa7d619da7d4f85958f0a7ddca474
Marcos Vanetta <marcosvanetta@gmail.com>
2012-02-12 17:54:29 -0300

    the files

---
+++ config/schedule.rb
@@ -0,0 +1,22 @@
+require 'yaml'
+
+current_path = Dir.pwd
+recipes_path = current_path + "/recipes"
+log_path = current_path 
+
+CONFIG = YAML.load_file(current_path + "/config/config.yml")
+recipes = CONFIG['recipes']
+
+set :output, log_path + "/cron_log.log"
+
+every 1.day, :at => "4.30 am" do
+  command "sudo service lighttpd stop"
+
+  recipes.each do |recipe|
+    command "ebook-convert #{recipes_path}/#{recipe} #{recipe}.mobi"
+    command "calibre-smtp --attachment #{recipe}.mobi --relay smtp.gmail.com --port 587 --username #{CONFIG['username']} --password \"#{CONFIG['password']}\" --encryption-method TLS #{CONFIG['email']} #{CONFIG['kindle_email']}"
+    command "rm #{recipe}.mobi"
+  end
+
+  command "sudo service lighttpd start"
+end
