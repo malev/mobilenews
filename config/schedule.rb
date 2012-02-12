6859a17414bb45071dc5ec5e47d3d67987e8bd16
Marcos Vanetta <marcosvanetta@gmail.com>
2012-02-12 18:03:11 -0300

    improving

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,7 +10,7 @@ recipes = CONFIG['recipes']
 set :output, log_path + "/cron_log.log"
 
 every 1.day, :at => "4.30 am" do
-  command "sudo service lighttpd stop"
+  command "service lighttpd stop"
 
   recipes.each do |recipe|
     command "ebook-convert #{recipes_path}/#{recipe} #{recipe}.mobi"
@@ -18,12 +18,14 @@ every 1.day, :at => "4.30 am" do
     command "rm #{recipe}.mobi"
   end
 
-  command "sudo service lighttpd start"
+  command "service lighttpd start"
 end
 
 recipe = "ieco.recipe"
 every :Sunday, :at => '5 am' do
+  command "service lighttpd stop"
   command "ebook-convert #{recipes_path}/#{recipe} #{recipe}.mobi"
   command "calibre-smtp --attachment #{recipe}.mobi --relay smtp.gmail.com --port 587 --username #{CONFIG['username']} --password \"#{CONFIG['password']}\" --encryption-method TLS #{CONFIG['email']} #{CONFIG['kindle_email']}"
   command "rm #{recipe}.mobi"
+  command "service lighttpd start"
 end
