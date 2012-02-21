a942b6f506d5d60be6516149d98d95b090aa8f8c
Marcos Vanetta <marcosvanetta@gmail.com>
2012-02-21 11:17:41 -0300

    new version

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# calibre server configuration
+Mobilenews
+==========
 
-whenever
-whenever --update-crontab
+Small program that creates cron tasks to deliver the newspaper directly into your kindle. Of course, it requires calibre. In this case it sends: a daily newspaper, a magazine that's delivered on sundays and my Read it later list every hour.
+
+Use
+---
+Clone it anywhere, create config.yml with the correct information and then run:
+
+> bundle
+> whenever --update-crontab
+
+TODO
+----
+* create a new coolest syntax
+* Add an option to deliver now everything.
+
+More information
+----------------
+You can find more info about me and my projects on my personal [blog](https://blog.malev.com.ar)
+
+License
+-------
+mobilenews is Copyright © 2011 malev.com.ar . It is free software, and may be redistributed under the terms specified in the LICENSE file.
--- config/schedule.rb
+++ config/schedule.rb
@@ -10,26 +10,23 @@ recipes = CONFIG['recipes']
 set :output, log_path + "/cron_log.log"
 
 every 1.day, :at => "4.30 am" do
-  command "service lighttpd stop"
-
   recipes.each do |recipe|
-    command "ebook-convert #{recipes_path}/#{recipe} #{recipe}.mobi"
+    command "ebook-convert #{recipes_path}/#{recipe} #{File.join(current_path, recipe + ".mobi")}"
     command "calibre-smtp --attachment #{recipe}.mobi --relay smtp.gmail.com --port 587 --username \"#{CONFIG['email']}\" --password \"#{CONFIG['password']}\" --encryption-method TLS #{CONFIG['email']} #{CONFIG['kindle_email']} \"\""
-    command "rm #{recipe}.mobi"
+    command "rm #{File.join(current_path, recipe + ".mobi")}"
   end
-
-  command "service lighttpd start"
 end
 
 recipe = "ieco.recipe"
 every :Sunday, :at => '5 am' do
-  command "service lighttpd stop"
-  command "ebook-convert #{recipes_path}/#{recipe} #{recipe}.mobi"
+  command "ebook-convert #{recipes_path}/#{recipe} #{File.join(current_path, recipe + ".mobi")}"
   command "calibre-smtp --attachment #{recipe}.mobi --relay smtp.gmail.com --port 587 --username \"#{CONFIG['email']}\" --password \"#{CONFIG['password']}\" --encryption-method TLS #{CONFIG['email']} #{CONFIG['kindle_email']} \"\""
-  command "rm #{recipe}.mobi"
-  command "service lighttpd start"
+  command "rm #{File.join(current_path, recipe + ".mobi")}"
 end
 
-#every 1.hour do
-#  command "ebook-convert recipes/readitlater.recipe read.mobi --username #{CONFIG['ril_username']} --password #{CONFIG['ril_password']}"
-#end
+recipe = "readitlater.recipe"
+every 1.hour do
+  command "ebook-convert recipes/readitlater.recipe #{File.join(current_path, recipe + ".mobi")} --username #{CONFIG['ril_username']} --password #{CONFIG['ril_password']}"
+  command "calibre-smtp --attachment #{recipe}.mobi --relay smtp.gmail.com --port 587 --username \"#{CONFIG['email']}\" --password \"#{CONFIG['password']}\" --encryption-method TLS #{CONFIG['email']} #{CONFIG['kindle_email']} \"\""
+  command "rm #{File.join(current_path, recipe + ".mobi")}"
+end
